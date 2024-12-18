4dd399def3cc187861616e7e4f9dcd28ca054764
Duccio Rocchini <ducciorocchini@gmail.com>
2024-12-18 14:53:18 +0100

    Update complex.R

--- R/complex.R
+++ R/complex.R
@@ -5,8 +5,7 @@ complex <- function(x){
   else if(x<0){
     print("This is a negative number")
    }
-  else if(x==0){
+  else
    print("This is just zero! Nor positive nor negative! Have you studied maths at school?")
-   }
   }
 
